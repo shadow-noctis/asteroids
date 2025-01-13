e4717919ef43970aa1df9e814dbef0f32bea9216
cursed_noctis <noctisinmementos@gmail.com>
2025-01-13 13:07:47 +0200

    Player moves, grouping done

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #updatable = pygame.sprite.Group()
-    #drawable = pygame.sprite.Group()
-    #Player.containers = (updatable, drawable)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,8 +19,14 @@ def main():
 
         screen.fill((0,0,0))
 
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+        
+        #updatable.update(dt)
+        #drawable.draw(screen)
 
 
         pygame.display.flip()
